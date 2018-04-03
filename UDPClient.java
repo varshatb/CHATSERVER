3e5f61d57e225f399e4ff8a9d87e7f30ccb6b373
Yami <sruthisathyan001@gmail.com>
2018-04-03 13:22:39 +0530

    u3

--- UDPClient.java
+++ UDPClient.java
@@ -3,3 +3,6 @@ import java.net.*;
 class UDPClient
 {
    public static void main(String args[]) throws Exception
+ {
+      BufferedReader inFromUser =
+      new BufferedReader(new InputStreamReader(System.in));
